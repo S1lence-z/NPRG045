2cfd0dc84b8831a2b5adbe554fed4c4e4101fdad
S1lence-z <j3.zelenka@gmail.com>
2024-07-19 11:32:53 +0200

    chore: add is_active column to sensor model

--- backend/sensors/models.py
+++ backend/sensors/models.py
@@ -6,6 +6,7 @@ class Sensor(models.Model):
     port_name = models.CharField(max_length=50)
     hwid = models.CharField(max_length=50)
     port_description = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.port_name} - {self.hwid}"
