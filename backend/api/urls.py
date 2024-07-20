1cd9522331dd30e44cf090c8a9c0ee37c55b79f2
S1lence-z <j3.zelenka@gmail.com>
2024-07-20 15:44:15 +0200

    Closes #2: refactor api endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sensors/available/', views.SensorViewSet.as_view({'get': 'available'}), name='available-sensors'),
-    path('sensors/active/', views.SensorViewSet.as_view({'get': 'active'}), name='active-sensors'),
-    path('sensors/active/<int:pk>', views.SensorViewSet.as_view({'post': 'set_active'}), name='set-sensor-active'),
-    path('sensors/inactive/', views.SensorViewSet.as_view({'get': 'inactive'}), name='inactive-sensors'),
-    path('sensors/inactive/<int:pk>', views.SensorViewSet.as_view({'post': 'set_inactive'}), name='set-sensor-inactive'),
+    ### PORTS
+    path('ports/', views.PortsView.as_view(), name='available-sensors'),
+    path('ports/<str:port_name>', views.PortsView.as_view(), name='connect-sensor'),
+    ### SENSORS
+    path('sensors/', views.SensorViewSet.as_view({'get': 'list'}), name='list-sensors'),
+    path('sensors/<int:pk>', views.SensorViewSet.as_view({'patch': 'change_connection_status'}), name='update-sensor-status'),
+    path('sensors/<int:pk>', views.SensorViewSet.as_view({'delete': 'delete'}), name='delete-sensor'),
+    path('sensors/connected/', views.SensorViewSet.as_view({'get': 'get_connected'}), name='get-connected-sensors'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,50 +1,63 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from sensors.models import Sensor
+from rest_framework.views import APIView
 from .serializers import SensorSerializer
+from sensors.models import Sensor
+from sensors.serializers import PortInformationSerializer
+from sensors.utils import get_serial_ports, filter_ports
 
 # Create your views here.
+
+class PortsView(APIView):
+    def get(self, request):
+        ports = get_serial_ports()
+        enhanced_ports = filter_ports(ports, enhanced=True)
+        serializer = PortInformationSerializer(enhanced_ports, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, port_name):
+        sensor = Sensor.objects.filter(port_name=port_name)
+        if sensor.exists():
+            sensor = sensor.first()
+            sensor.is_connected = True
+            sensor.save()
+            return Response({'message': f'Known sensor on port {port_name} conncted'}, status=status.HTTP_200_OK)
+        else:
+            ports = get_serial_ports()
+            for port in ports:
+                if port.name == port_name:
+                    sensor = Sensor.objects.create(port_name=port.name, port_description=port.description, hwid=port.device_hwid, is_connected=True)
+                    sensor.save()
+                    return Response({'message': f'New sensor on port {port_name} connected'}, status=status.HTTP_200_OK)
+            return Response({'message': f'Sensor on port {port_name} not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class SensorViewSet(viewsets.ModelViewSet):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
     
-    @action(detail=False, methods=['get'])
-    def available(self, request):
+    def list(self, request):
         sensors = Sensor.objects.all()
-        serializer = self.get_serializer(sensors, many=True)
-        return Response(serializer.data)
+        serializer = SensorSerializer(sensors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    @action(detail=False, methods=['get'])
-    def active(self, request):
-        active_sensors = Sensor.objects.filter(is_active=True)
-        serializer = self.get_serializer(active_sensors, many=True)
-        return Response(serializer.data)
-    
-    @action(detail=True, methods=['post'])
-    def set_active(self, request, pk):
+    def delete(self, request, pk):
         try:
             sensor = Sensor.objects.get(pk=pk)
-            sensor.is_active = True
-            sensor.save()
-            Sensor.objects.exclude(pk=pk).update(is_active=False)
-            return Response({'status': f'Sensor with id {sensor.id} activated.'}, status=status.HTTP_200_OK)
+            sensor.delete()
+            return Response({'message': f'Sensor on port {sensor.port_name} deleted'}, status=status.HTTP_200_OK)
         except Sensor.DoesNotExist:
-            return Response({'status': f'Sensor with id {pk} not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Sensor not found'}, status=status.HTTP_404_NOT_FOUND)
         
     @action(detail=False, methods=['get'])
-    def inactive(self, request):
-        inactive_sensors = Sensor.objects.filter(is_active=False)
-        serializer = self.get_serializer(inactive_sensors, many=True)
-        return Response(serializer.data)
+    def get_connected(self, request):
+        sensors = Sensor.objects.filter(is_connected=True)
+        serializer = SensorSerializer(sensors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    @action(detail=True, methods=['post'])
-    def set_inactive(self, request, pk):
-        try:
-            sensor = Sensor.objects.get(pk=pk)
-            sensor.is_active = False
-            sensor.save()
-            return Response({'status': f'Sensor with id {sensor.id} deactivated.'}, status=status.HTTP_200_OK)
-        except Sensor.DoesNotExist:
-            return Response({'status': f'Sensor with id {pk} not found.'}, status=status.HTTP_404_NOT_FOUND)
+    @action(detail=True, methods=['patch'])
+    def change_connection_status(self, request, pk):
+        sensor = Sensor.objects.get(pk=pk)
+        sensor.is_connected = not sensor.is_connected
+        sensor.save()
+        return Response({'message': f'Sensor on port {sensor.port_name} is now {"active" if sensor.is_connected else "inactive"}'}, status=status.HTTP_200_OK)
