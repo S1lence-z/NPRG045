43030434cc4d517790e741823c6c39ed1ecc4dbd
S1lence-z <j3.zelenka@gmail.com>
2024-07-21 19:10:39 +0200

    chore: simplify websockets

--- backend/ws_app/consumers.py
+++ backend/ws_app/consumers.py
@@ -5,8 +5,7 @@ from channels.generic.websocket import WebsocketConsumer
 class MessageType(Enum):
     CONNECTION_ESTABLISHED = "connection_established"
     CONNECTION_CLOSED = "connection_closed"
-    DATA = "data"
-    WARNING = "warning"
+    PORT_CHANGE = "port_change"
 
 class WebSocketConsumer(WebsocketConsumer):
     def connect(self):
@@ -18,26 +17,12 @@ class WebSocketConsumer(WebsocketConsumer):
     
     def disconnect(self, close_code):
         print(f'Connection closed. Close code: {close_code}')
-        pass
+        self.send(text_data=json.dumps({
+            'type': MessageType.CONNECTION_CLOSED.value,
+            'message': 'Connection closed with the server.'
+        }))
     
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message_type = text_data_json['type']
-        message_content = text_data_json['message']
-        
-        if message_type == MessageType.DATA.value:
-            self.send(text_data=json.dumps({
-                'type': MessageType.DATA.value,
-                'message': message_content
-            }))
-        elif message_type == MessageType.WARNING.value:
-            self.send(text_data=json.dumps({
-                'type': MessageType.WARNING.value,
-                'message': message_content
-            }))
-        elif message_type == MessageType.CONNECTION_CLOSED.value:
-            self.send(text_data=json.dumps({
-                'type': MessageType.CONNECTION_CLOSED.value,
-                'message': message_content
-            }))
-            self.close()
+        request_type = text_data_json['type']
+        request_content = text_data_json['message']
--- backend/ws_app/routing.py
+++ backend/ws_app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path('ws/data/', consumers.WebSocketConsumer.as_asgi()),
+    re_path('ws/connections/', consumers.WebSocketConsumer.as_asgi()),
 ]
