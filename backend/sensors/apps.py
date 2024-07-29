11a6dcac5de84275351d48eba94e9d1cf9686320
S1lence-z <j3.zelenka@gmail.com>
2024-07-29 23:34:08 +0200

    feat: add a method to reset the sensor model

--- backend/sensors/apps.py
+++ backend/sensors/apps.py
@@ -1,11 +1,22 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class SensorsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sensors'
-
-    def ready(self):
+        
+    def _instantiate_sensor_client_manager(self):
         from .sensor_management.sensor_client_manager import SensorClientManager
         SensorClientManager.get_instance()
         print('SensorClientManager singleton created')
+        
+    @receiver(post_migrate)
+    def _prepare_db(self, sender, **kwargs):
+        from sensors.models import Sensor
+        Sensor.objects.update(is_connected=False)
+        print('Successfully reset all connected sensors')
+
+    def ready(self):
+        self._instantiate_sensor_client_manager()
+        post_migrate.connect(self._prepare_db, sender=self)
