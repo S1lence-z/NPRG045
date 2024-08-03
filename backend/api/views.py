0e171003af7b26bc2a9ceff12b6c2dae2ae2952e
S1lence-z <j3.zelenka@gmail.com>
2024-08-03 15:24:09 +0200

    Closes #9: clear and simplify endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,12 +15,12 @@ class PortsAPIView(APIView):
         return Response(data=serializer.data, status=status.HTTP_200_OK)
     
 class SensorListCreateAPIView(APIView):
-    def get(self, request): 
+    def get(self, request):
         sensors = Sensor.objects.all()
         serializer = SensorSerializer(sensors, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
     
-    def post(self, request):        
+    def post(self, request):
         requested_port_name = request.data.get('name')
         requested_description = request.data.get('description')
         requested_device_hwid = request.data.get('device_hwid')
@@ -29,7 +29,6 @@ class SensorListCreateAPIView(APIView):
         if sensor:
             sensor.is_connected = True
             sensor.save()
-            #! the signal handler will update the sensor client manager
             serializer = SensorSerializer(sensor)
             return Response(data={
                 'message': f'Known sensor with id {sensor.id} on port {requested_port_name} connected',
@@ -43,14 +42,13 @@ class SensorListCreateAPIView(APIView):
             is_connected=True
         )
         sensor.save()
-        #! the signal handler will update the sensor client manager
         serializer = SensorSerializer(sensor)
         return Response(data={
             'message': f'New sensor with id {sensor.id} on port {requested_port_name} connected',
             'sensor': serializer.data}, status=status.HTTP_201_CREATED)
         
 class SensorDetailAPIView(APIView):
-    def put(self, request, pk):        
+    def put(self, request, pk):
         try:
             sensor = Sensor.objects.get(pk=pk)
         except Sensor.DoesNotExist:
@@ -60,17 +58,15 @@ class SensorDetailAPIView(APIView):
         serializer = SensorSerializer(sensor, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            #! the signal handler will update the sensor client manager
             return Response(data={
                 'message': f'Sensor with id {pk} on port {sensor.port_name} updated', 
                 'sensor': serializer.data}, status=status.HTTP_200_OK)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-    def delete(self, request, pk):        
+    def delete(self, request, pk):
         try:
             sensor = Sensor.objects.get(pk=pk)
             sensor.delete()
-            #! the signal handler will update the sensor client manager
             return Response(data={
                 'message': f'Sensor with id {pk} on port {sensor.port_name} deleted',
                 }, status=status.HTTP_200_OK)
